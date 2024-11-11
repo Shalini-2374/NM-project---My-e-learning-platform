29e9e1bb0af0641e265900ebb0c8ba9bf96a5094
Shalini-2374 <146435716+Shalini-2374@users.noreply.github.com>
2024-11-11 22:07:03 +0530

    Add files via upload

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,1 +1,36 @@
-.
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', // Adjust this to your main JavaScript file path
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader', // Make sure you have babel-loader installed
+        },
+      },
+      {
+        test: /\.css$/, // If you are using CSS, include this rule
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your HTML file
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 5000,
+  },
+};
